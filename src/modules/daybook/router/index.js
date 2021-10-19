41bb3da9fe177cebb6000b7f97be3453a634b666
Miriam Valera <miriamvdaw@gmail.com>
2021-10-19 12:51:20 +0200

    add link to home and modular structure

---
+++ src/modules/daybook/router/index.js
@@ -0,0 +1,10 @@
+
+
+export default {
+
+    name: 'daybook',
+    component: () => import(/* webpackChunkName: "daybook" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
+    children: [
+        
+    ]   
+}
