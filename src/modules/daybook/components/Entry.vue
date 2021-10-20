ea5178435fb031d37b5b58f2def350c0ff10d45b
Miriam Valera <miriamvdaw@gmail.com>
2021-10-20 11:21:49 +0200

    add component EntryView with buttons, img and router

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,6 +1,7 @@
 <template>
     <div 
         class="entry-container mb-3 pointer p-2"
+        @click="$router.push({name: 'entry', params: { id: 10 }})"
     >
         <!-- Título -->
         <div class="entry-title d-flex">
