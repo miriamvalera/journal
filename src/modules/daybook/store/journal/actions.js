b2b9cdd083b847482dd9879411d84d2a132b0754
Miriam Valera <miriamvdaw@gmail.com>
2022-01-24 14:36:49 +0100

    added entries in actions

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -6,7 +6,13 @@ import journalApi from '@/api/journalApi'
 
 export const loadEntries = async (/*{ commit }*/) => {
     const { data } = await journalApi.get('/entries.json')
-    console.log(data)
+    const entries = []
+    for( let id of Object.keys(data) ) {
+        entries.push({
+            id, 
+            ...data[id]
+        })
+    }
 }
 
 export const updateEntry = async (/*{ commit }*/) => {
